c177938878537b9861e222ef7ab11f5659e4e6d1
xshua06 <xshua06@163.com>
2016-05-10 21:06:48 +0800

    fixed  checked -> defaultChecked

--- src/js/Switch.js
+++ src/js/Switch.js
@@ -44,7 +44,7 @@ var Switch = React.createClass({
           onChange={onValueChange.bind(this)}
           name={name}
           disabled={disabled}
-          checked={checked}
+          defaultChecked={checked}
           type="checkbox"
           ref="field"
         />
